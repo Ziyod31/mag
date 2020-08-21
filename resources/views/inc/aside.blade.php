379b6ae6cbb05d77bf9795f7304023d0510028fb
Ziyod31 <ziyod_31@mail.ru>
2020-08-21 10:12:34 +0500

    Database Migration

---
+++ resources/views/inc/aside.blade.php
@@ -0,0 +1,61 @@
+<article class="filter-group">
+	<header class="card-header">
+		<a href="#" data-toggle="collapse" data-target="#collapse_1" aria-expanded="true" class="">
+			<i class="icon-control fa fa-chevron-down"></i>
+			<h6 class="title">Product type</h6>
+		</a>
+	</header>
+	<div class="filter-content collapse show" id="collapse_1" style="">
+		<div class="card-body">
+			<ul class="list-menu">
+				<li><a href="#">People  </a></li>
+			</ul>
+
+		</div> <!-- card-body.// -->
+	</div>
+</article> <!-- filter-group  .// -->
+
+<article class="filter-group">
+	<header class="card-header">
+		<a href="#" data-toggle="collapse" data-target="#collapse_2" aria-expanded="true" class="">
+			<i class="icon-control fa fa-chevron-down"></i>
+			<h6 class="title">Brands </h6>
+		</a>
+	</header>
+	
+	<div class="filter-content collapse show" id="collapse_2" style="">
+		<div class="card-body">
+			<label class="custom-control custom-checkbox">
+				<input type="checkbox" checked="" class="custom-control-input">
+				<div class="custom-control-label">Mercedes  
+					<b class="badge badge-pill badge-light float-right">120</b>  </div>
+				</label>
+			</div> <!-- card-body.// -->
+		</div>
+	</article> <!-- filter-group .// -->
+
+	<article class="filter-group">
+		<header class="card-header">
+			<a href="#" data-toggle="collapse" data-target="#collapse_3" aria-expanded="true" class="">
+				<i class="icon-control fa fa-chevron-down"></i>
+				<h6 class="title">Price range </h6>
+			</a>
+		</header>
+
+		<div class="filter-content collapse show" id="collapse_3" style="">
+			<div class="card-body">
+				<input type="range" class="custom-range" min="0" max="100" name="">
+				<div class="form-row">
+					<div class="form-group col-md-6">
+						<label>Min</label>
+						<input class="form-control" placeholder="$0" type="number">
+					</div>
+					<div class="form-group text-right col-md-6">
+						<label>Max</label>
+						<input class="form-control" placeholder="$1,0000" type="number">
+					</div>
+				</div> <!-- form-row.// -->
+				<button class="btn btn-block btn-primary">Apply</button>
+			</div><!-- card-body.// -->
+		</div>
+</article> <!-- filter-group .// -->
---
+++ resources/views/inc/brand.blade.php
@@ -0,0 +1,38 @@
+<div class="container">
+	<header class="section-heading">
+		<h3 class="section-title">Our Brands</h3>
+	</header><!-- sect-heading -->
+
+	<div class="row">
+		<div class="col-md-2 col-6">
+			<figure class="box item-logo">
+				<a href="#"><img src="images/logos/logo1.png"></a>
+			</figure> <!-- item-logo.// -->
+		</div> <!-- col.// -->
+		<div class="col-md-2  col-6">
+			<figure class="box item-logo">
+				<a href="#"><img src="images/logos/logo2.png"></a>
+			</figure> <!-- item-logo.// -->
+		</div> <!-- col.// -->
+		<div class="col-md-2  col-6">
+			<figure class="box item-logo">
+				<a href="#"><img src="images/logos/logo3.png"></a>
+			</figure> <!-- item-logo.// -->
+		</div> <!-- col.// -->
+		<div class="col-md-2  col-6">
+			<figure class="box item-logo">
+				<a href="#"><img src="images/logos/logo4.png"></a>
+			</figure> <!-- item-logo.// -->
+		</div> <!-- col.// -->
+		<div class="col-md-2  col-6">
+			<figure class="box item-logo">
+				<a href="#"><img src="images/logos/logo5.png"></a>
+			</figure> <!-- item-logo.// -->
+		</div> <!-- col.// -->
+		<div class="col-md-2  col-6">
+			<figure class="box item-logo">
+				<a href="#"><img src="images/logos/logo2.png"></a>
+			</figure> <!-- item-logo.// -->
+		</div> <!-- col.// -->
+	</div> <!-- row.// -->
+</div><!-- container // -->
